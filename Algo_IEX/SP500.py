94b424fc24a03e8a045563845dcefd50d039e2ba
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2021-01-07 23:00:03 -0400

    Got s&p 500 working, fixed 400 errors

--- Algo_IEX/SP500.py
+++ Algo_IEX/SP500.py
@@ -6,18 +6,29 @@ import xlsxwriter
 from secrets import IEX_CLOUD_API_TOKEN
 
 
-stocks = pd.read_csv('./Data-Science/Algo_IEX/sp_500_stocks.csv')
+stocks = pd.read_csv('sp_500_stocks.csv')
 
 symbol = 'AAPL'
 api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
 data = requests.get(api_url).json()
 
+ticker = data['symbol']
 price = data['latestPrice']
 market_cap= data['marketCap']
+name = data['companyName']
+market_cap = data['marketCap']
+peratio = data['peRatio']
+per_change = data['changePercent']
+volume = data['volume']
+price_high = data['week52High']
+price_low = data['week52Low']
+shares_buy = 100
 
-my_columns = ['Ticker', 'Stock Price', 'Maret Capitalization', 'Number of Shares to Buy']
-final_dataframe = pd.DataFrame([[0,0,0,0,]], columns = my_columns)
+stock_info = [{ticker, name, price, market_cap, per_change, peratio, volume, price_high, price_low, shares_buy}]
+
+
+
+my_columns = ['Ticker', 'Company Name','Stock Price', 'Market Capitalization', 'Percent Change (Day)', 'PE Ratio', 'Volume', '52 High', '52 Low', 'Number of Shares to Buy']
+final_dataframe = pd.DataFrame(stock_info, columns = my_columns)
 print(final_dataframe)
-
-
 
