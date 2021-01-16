f10f435d1493b5b0b20d455eef98c6a4d8ffbfb2
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2021-01-15 21:22:55 -0400

    BTC Analysis

---
+++ BTC_Analysis/Btc_regression.py
@@ -0,0 +1,16 @@
+import numpy as py
+import pandas as pd 
+import xlsxwriter 
+
+btc = pd.read_csv('./Data-Science/BTC_Analysis/BTC-USD.csv')
+
+my_columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
+
+dataframe = pd.DataFrame(columns= my_columns)
+
+final_df = dataframe.append(btc, ignore_index = True)
+
+final_df["Day_percent_change"] = ( final_df["Close"] - final_df["Open"] ) / final_df["Open"]
+
+print(final_df)
+
