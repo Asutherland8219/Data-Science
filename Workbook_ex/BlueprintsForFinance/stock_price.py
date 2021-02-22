600a370315106aee5d0fcba926bf1f9acb6a7165
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2021-02-22 07:51:46 -0400

    Create tests for testing best fit model

--- Workbook_ex/BlueprintsForFinance/stock_price.py
+++ Workbook_ex/BlueprintsForFinance/stock_price.py
@@ -45,7 +45,7 @@ from pandas.plotting import scatter_matrix
 from statsmodels.graphics.tsaplots import plot_acf
 
 #load the data
-stk_tickers = ['MSFT', 'IBM', 'GOOGL']
+stk_tickers = ['MSFT', 'AAPL', 'GOOGL']
 ccy_tickers = ['DEXJPUS', 'DEXUSUK']
 idx_tickers = ['SP500', 'DJIA', 'VIXCLS']
 
@@ -60,12 +60,12 @@ Y = np.log(stk_data.loc[:, ('Adj Close', 'MSFT')]).diff(return_period).\
 shift(-return_period)
 Y.name = Y.name[-1]+'_pred'
 
-X1 = np.log(stk_data.loc[:,('Adj Close', ('GOOGL', 'IBM'))]).diff(return_period)
+X1 = np.log(stk_data.loc[:,('Adj Close', ('GOOGL', 'AAPL'))]).diff(return_period)
 X1.columns = X1.columns.droplevel()
 X2 = np.log(ccy_data).diff(return_period)
 X3 = np.log(idx_data).diff(return_period)
 
-X4 = pd.concat([np.log(stk_data.loc[:, ('Adj Close', 'MSFT')]).diff(i) \
+X4 = pd.concat([np.log(stk_data.loc[:, ('Adj Close', 'AAPL')]).diff(i) \
     for i in [return_period, return_period * 3, \
         return_period*6, return_period*12]], axis=1).dropna()
 X4.columns = ['MSFT_DT', 'MSFT_3DT', 'MSFT_6DT', 'MSFT_12DT']
@@ -76,6 +76,85 @@ dataset = pd.concat([Y, X], axis=1).dropna().iloc[::return_period, :]
 Y = dataset.loc[:, Y.name]
 X = dataset.loc[:, X.columns]
 
-print(dataset.head())
+''' Data visualization '''
+# Correlation Matrix chart 
+correlation = dataset.corr()
+pyplot.figure(figsize=(15,15))
+pyplot.title('Correlation Matrix')
+sns.heatmap(correlation, vmax = 1, square=True, annot=True, cmap='cubehelix')
+pyplot.show()
 
+# Scatter plot 
+pyplot.figure(figsize=(15,15))
+scatter_matrix(dataset, figsize=(12,12))
+pyplot.show()
+
+# Time Series Analysis 
+res = sm.tsa.seasonal_decompose(Y, period=52)
+fig = res.plot()
+fig.set_figheight(8)
+fig.set_figwidth(15)
+pyplot.show()
+
+''' Create the train, test, split and ML Model ''' 
+validation_size = 0.2
+train_size = int(len(X) * (1-validation_size))
+X_train, X_test = X[0:train_size], X[train_size:len(X)]
+Y_train, Y_test = Y[0:train_size], Y[train_size:len(Y)]
+
+#Cross validation to test hyperparameters
+num_folds = 10
+scoring = 'neg_mean_squared_error'
+
+# Model Selection 
+## Regression and tree regression algorithms 
+models = []
+models.append(('LR', LinearRegression()))
+models.append(('LASSO', Lasso()))
+models.append(('EN', ElasticNet()))
+models.append(('KNN', KNeighborsRegressor()))
+models.append(('CART', DecisionTreeRegressor()))
+models.append(('SVR', SVR()))
+
+## Neural NEtwork Algorithms
+models.append(('MLP', MLPRegressor()))
+
+## Ensemble Models 
+
+### Boosting Methods 
+models.append(('ABR', AdaBoostRegressor()))
+models.append(('GBR',GradientBoostingRegressor()))
+
+### Bagging Methods 
+models.append(('RFR', RandomForestRegressor()))
+models.append(('ETR', ExtraTreesRegressor()))
+
+''' Tuning and Results of Cross Validation ''' 
+names = []
+kfold_results = []
+test_results = []
+train_results = []
+for name, model in models :
+    names.append(name)
+    ## K-Fold Analysis
+    kfold = KFold(n_splits=num_folds)
+    ## Convert MSE to positive, lower = better
+    cv_results = -1*cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    kfold_results.append(cv_results)
+    # Full training period 
+    res = model.fit(X_train, Y_train)
+    train_result = mean_squared_error(res.predict(X_train), Y_train)
+    train_results.append(train_result)
+    # Test results 
+    test_result = mean_squared_error(res.predict(X_test), Y_test)
+    test_results.append(test_result)
+
+### Compare CV results 
+fig = pyplot.figure()
+fig.suptitle('Algorithm Comparison: KFold results')
+ax = fig.add_subplot(111)
+pyplot.boxplot(kfold_results)
+ax.set_xticklabels(names)
+fig.set_size_inches(15,8)
+pyplot.show()
 
