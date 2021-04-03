74cf4b8117c4e6f5bbc06b60733b76eefd95947a
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2021-04-03 09:11:42 -0300

    Started case study 3, risk tolerance and robo investing

---
+++ Workbook_ex/BlueprintsForFinance/robo_advisor_risk.py
@@ -0,0 +1,52 @@
+''' Modeling investor Risk Tolerance and enabling a ML ROBO advisor '''
+
+''' Function and modules for the Supervised Regression Models '''
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor 
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.neural_network import MLPRegressor
+
+''' Function and modules for Data Analysis and Model Evaluation '''
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2, f_regression
+
+''' Function and modules for deep learning models '''
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import SGD
+from keras.layers import LSTM
+from keras.wrappers.scikit_learn import KerasRegressor
+
+'''Function and modules for  time series models'''
+from statsmodels.tsa.arima_model import ARIMA
+import statsmodels.api as sm 
+
+'''Function and modules for data preparation and visualization'''
+
+# standard data science imports
+import numpy as np
+import pandas as pd 
+import pandas_datareader.data as web
+from matplotlib import pyplot
+from pandas.plotting import scatter_matrix
+import seaborn as sns 
+from sklearn.preprocessing import StandardScaler
+from pandas.plotting import scatter_matrix
+from statsmodels.graphics.tsaplots import plot_acf
+
+# load the dataset
+dataset = pd.read_excel('SCFP2009panel.xlsx')
+
+dataset.shape
