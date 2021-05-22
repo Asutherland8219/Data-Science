4bec693e8704d413c5b89e5826c210535065f764
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2021-05-22 11:32:56 -0300

    Almost ready to place orders, API key needed.

--- Binance_bot/Eth_model/bot.py
+++ Binance_bot/Eth_model/bot.py
@@ -1,10 +1,23 @@
-import websocket
-
+import websocket, json, pprint, talib, numpy
+import config
+from binance.client import Client
+from binance.enums import *
 ## <symbol> is where you put the asset you want to trade, in this instance its ETHUSD
 ## <interval> is the time interval between prices 
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
+RSI_Period = 14
+# RSI_Period = 30  , for BTC in a previous test, RSI 30 was the strongest correlated predictable variable
 
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
+TRADE_SYMBOL = 'ETHUSD'
+TRADE_QUANTITY = 0.05
+
+closes = []
+in_position = False
+
+client = Client(config.API_KEY, config.API_SECRET, tld='cad')
 ## btcusdt is bitcoin, ltcusdt is litecoin, 
 
 def on_open(ws):
@@ -15,6 +28,44 @@ def on_close(ws):
 
 def on_message(ws, message):
     print('received message')
+    json_message = json.loads(message)
+    pprint.pprint(json_message)
+
+    candle = json_message['k']
+    is_candle_closed = candle['x']
+    close = candle['c']
+
+
+    if is_candle_closed:
+        print('candle closed at {}'.format(close))
+        closes.append(float(close))
+        print('closes')
+        print(closes)
+
+        if len(closes) > RSI_Period:
+            np_closes = numpy.arry(closes)
+            rsi = talib.RSI(np_closes, RSI_Period)
+            print("all rsis calculated so far")
+            print(rsi)
+            last_rsi = rsi[-1]
+            print("the current rsi is {}".format(last_rsi))
+
+            if last_rsi > RSI_OVERBOUGHT:
+                print("Sell! Sell! Sell!")
+            else: 
+                print("It is overbought, but we dont own any.")
+
+            if last_rsi < RSI_OVERSOLD:
+                if in_position:
+                    print("It is oversold, but you already own it, nothing to do")
+                else:
+                    print("Buy! Buy! Buy!")
+
+
+
+
+
+
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 ws.run_forever()
