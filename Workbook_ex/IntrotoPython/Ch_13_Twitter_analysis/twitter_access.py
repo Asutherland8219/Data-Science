fb0f198cbc45d75ea3109cbcd6b8fb3c0518fa63
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2021-07-09 07:52:30 -0300

    Word cloud result; need to filter emojis

--- Workbook_ex/IntrotoPython/Ch_13_Twitter_analysis/twitter_access.py
+++ Workbook_ex/IntrotoPython/Ch_13_Twitter_analysis/twitter_access.py
@@ -3,4 +3,30 @@ import secrets
 
 auth = tweepy.OAuthHandler(secrets.apikey, secrets.api_s_key)
 
-auth.set_access_token()
+auth.set_access_token(secrets.access_token, secrets.access_token_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+# wait on rate limit = wait 15 mins each time it hits api limit
+# wait on rate limit notify just enables it to show in shell 
+
+elon = api.get_user('elonmusk')
+
+print(elon.id)
+
+# elon.followers_count
+# elon.friends_count
+
+''' If you want to get information about yourself or your account use the following'''
+# me = api.me()
+# api.home_timeline()
+
+''' The textbook references how to get who someone follows or who follows them. That information is essentially useless so was omitted'''
+
+
+''' Getting recent Tweets'''
+elon_tweets = api.user_timeline(screen_name = 'elonmusk', count= 3)
+
+for tweet in elon_tweets:
+    print(f'{tweet.user.screen_name}: {tweet.text}')
+
---
+++ Workbook_ex/IntrotoPython/Ch_13_Twitter_analysis/twitter_search.py
@@ -0,0 +1,51 @@
+from textblob import TextBlob
+import tweepy
+import secrets
+
+auth = tweepy.OAuthHandler(secrets.apikey, secrets.api_s_key)
+
+auth.set_access_token(secrets.access_token, secrets.access_token_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+# wait on rate limit = wait 15 mins each time it hits api limit
+# wait on rate limit notify just enables it to show in shell 
+def print_tweets(tweets):
+    ''' For each status object in the tweets, display the screen and text. Then use textblob to translate to english'''
+    for tweet in tweets:
+        print(f'{tweet.user.screen_name}:', end=' ')
+
+        if 'en' in tweet.lang:
+            print(f'{tweet.text}')
+        elif 'und' not in tweet.lang:
+            print(f'\n ORIGINAL: {tweet.text}')
+            print(f'TRANSLATED: {TextBlob(tweet.text).translate()}')
+
+
+tweets = api.search(q='Oil', count=3)
+print_tweets(tweets)
+
+''' Search for a hashtag '''
+tweets = api.search(q='#itscominghome', count=10)
+print_tweets(tweets)
+
+
+''' Search for trends available; please note this is trendy places not topics '''
+trends_available = api.trends_available()
+print(len(trends_available))
+
+''' We can narrow down to a specific place or hashtag  '''
+world_trends = api.trends_place(id= 1)
+
+trends_list = world_trends[0]['trends']
+
+trends_list = [t for t in trends_list if t['tweet_volume']]
+
+from operator import itemgetter
+trends_list.sort(key=itemgetter('tweet_volume'), reverse=True)
+
+for trend in trends_list[:5]:
+    print(trend['name'])
+
+
+
