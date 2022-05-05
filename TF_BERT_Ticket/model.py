489c5ad083f90457acdc144cb7b01574653eaa1d
Alex Sutherland <asutherland8219@gmail.com>
2022-05-05 09:18:01 -0300

    Started TF model for ticket sorting

---
+++ TF_BERT_Ticket/model.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+from official.nlp import optimization  # to create AdamW optimizer
+
+import matplotlib.pyplot as plt
+
+tf.get_logger().setLevel('ERROR')
