1148102d4079808148adeb893968ed49d1ba0c0a
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2022-05-08 22:27:12 -0300

    Started the logic for text matching model  Please enter the commit message for your changes. Lines starting

--- NLP_text_match/text_match.py
+++ NLP_text_match/text_match.py
@@ -1,28 +1,48 @@
+from urllib import response
 import spacy 
 from spacy.matcher import PhraseMatcher
 from textblob import Sentence
+import csv 
+import json
+import numpy as np
 
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_md')
 phrase_matcher = PhraseMatcher(nlp.vocab)
 
 
 # this is where you would add the tickers
-phrases = []
-patterns = [nlp(text) for text in phrases]
+with open("NLP_text_match\sp_500_stocks.json", "r") as read_file:
+    data = json.load(read_file)
+    
+    data2 = json.loads(str(data))
+
+print(data2)
+
+# for i in data:
+#     response_set = []
+#     i_set = i
+#     response_set.append(i_set)
+#     for x in response_set:
+#         y = np.array(x)
+       
+# phrases = data
+# patterns = [nlp(text) for text in phrases]
 
 
-phrase_matcher.add('AI', None, *patterns)
+# phrase_matcher.add('AI', None, *patterns)
 
 # the html of the link
 #processed_article = html
 
-sentence = nlp(processed_article)
-matched_phrases = phrase_matcher(sentence)
 
-for match_id, start, end in matched_phrases:
-    string_id = nlp.vocab.strings[match_id]
-    span = sentence[start:end]
-    print(match_id, string_id, start, end, span.text)
+
+# sentence = nlp(processed_article)
+# matched_phrases = phrase_matcher(sentence)
+
+# for match_id, start, end in matched_phrases:
+#     string_id = nlp.vocab.strings[match_id]
+#     span = sentence[start:end]
+#     print(match_id, string_id, start, end, span.text)
     
 
 
--- TF_BERT_Ticket/model.py
+++ TF_BERT_Ticket/model.py
@@ -1,11 +1,62 @@
 import os
 import shutil
 
-import tensorflow as tf
+
 import tensorflow_hub as hub
 import tensorflow_text as text
 from official.nlp import optimization  # to create AdamW optimizer
+from keras.layers import LayerNormalization
+import tensorflow as tf
 
 import matplotlib.pyplot as plt
 
 tf.get_logger().setLevel('ERROR')
+print(tf.__version__)
+
+
+''' Testing the logic of a bert model in order to see if it will work with ticket tagging '''
+
+# this block 15-28 is not really useful to our model 
+url = 'https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz'
+
+dataset = tf.keras.utils.get_file('aclImdb_v1.tar.gz', url,
+                                  untar=True, cache_dir='.',
+                                  cache_subdir='')
+
+dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
+
+train_dir = os.path.join(dataset_dir, 'train')
+
+# remove unused folders to make it easier to load the data
+remove_dir = os.path.join(train_dir, 'unsup')
+shutil.rmtree(remove_dir)
+
+AUTOTUNE = tf.data.AUTOTUNE
+batch_size = 32
+seed = 42
+
+raw_train_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='training',
+    seed=seed)
+
+class_names = raw_train_ds.class_names
+train_ds = raw_train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+val_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='validation',
+    seed=seed)
+
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+test_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/test',
+    batch_size=batch_size)
+
+test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
